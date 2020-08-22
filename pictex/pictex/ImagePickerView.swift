237f6b0a42bbfa0a0390f46d74f7603e03dea54e
Len Huang <lendevelops@gmail.com>
2020-08-22 01:08:50 -0400

    complete cloudfront link display

---
+++ pictex/pictex/ImagePickerView.swift
@@ -0,0 +1,21 @@
+//
+//  ImagePickerView.swift
+//  pictex
+//
+//  Created by Len Huang on 8/21/20.
+//  Copyright © 2020 PicTex Technologies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerView()
+    }
+}
---
+++ pictex/pictex/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  pictex
+//
+//  Created by Len Huang on 8/21/20.
+//  Copyright © 2020 PicTex Technologies. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
